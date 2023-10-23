60c2b0909f061bc3db023a5fd1f6b72f569bb7c5
An2anetti <141183645+An2anetti@users.noreply.github.com>
2023-10-24 02:05:27 +0300

    Update DigitalMeasurement.py
    v.2.2.9

--- DigitalMeasurement.py
+++ DigitalMeasurement.py
@@ -1,5 +1,4 @@
-from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame, StringSetting, NumberSetting, ChoicesSetting
-from enum import Enum
+from saleae.analyzers import HighLevelAnalyzer, AnalyzerFrame
 
 class PacketMarkerState(Enum):
     STANDARD = 0
@@ -15,7 +14,7 @@ class PacketMarkerAnalyzer(HighLevelAnalyzer):
 
     def __init__(self):
         self.packet_duration = None
-        self.standard_packet_duration = 52  # Длительность стандартного пакета в микросекундах
+        self.standard_packet_duration = 11  # Длительность стандартного пакета в микросекундах
         self.nonstandard_packet_duration = 21  # Длительность нестандартного пакета в микросекундах
         self.packet_state = None
 
@@ -24,22 +23,23 @@ class PacketMarkerAnalyzer(HighLevelAnalyzer):
         packet_marker = 0 if state == PacketMarkerState.STANDARD else 1
         self.frames.append(AnalyzerFrame(start_time, duration, 'packet_marker', {'packet_marker': packet_marker}))
 
-    def decode(self, frame):
-        if self.packet_duration is None:
-            self.packet_duration = frame.duration
-            self.packet_state = PacketMarkerState.STANDARD
-        elif frame.duration == self.packet_duration:
-            if self.packet_state != PacketMarkerState.STANDARD:
-                self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
+    def decode(self):
+        for frame in self.data.frames:
+            if self.packet_duration is None:
+                self.packet_duration = frame.duration
                 self.packet_state = PacketMarkerState.STANDARD
-        elif frame.duration == self.nonstandard_packet_duration:
-            if self.packet_state != PacketMarkerState.NONSTANDARD:
-                self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
-                self.packet_state = PacketMarkerState.NONSTANDARD
-        else:
-            if self.packet_state is not None:
-                self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
-                self.packet_state = None
+            elif frame.duration == self.packet_duration:
+                if self.packet_state != PacketMarkerState.STANDARD:
+                    self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
+                    self.packet_state = PacketMarkerState.STANDARD
+            elif frame.duration == self.nonstandard_packet_duration:
+                if self.packet_state != PacketMarkerState.NONSTANDARD:
+                    self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
+                    self.packet_state = PacketMarkerState.NONSTANDARD
+            else:
+                if self.packet_state is not None:
+                    self.add_packet_marker(frame.start_time, frame.end_time, self.packet_state)
+                    self.packet_state = None
 
 # Здесь вы можете добавить свои собственные настройки, если необходимо
 settings = []
